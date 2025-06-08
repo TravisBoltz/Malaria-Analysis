9ff0b36e878c97f096496626fcb842370a0ee010
TravisBoltz <fariduoubdamusah@gmail.com>
2025-06-08 21:57:13 +0000

    feat: login page

---
+++ src/components/login-form.tsx
@@ -0,0 +1,83 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import { ChartLine, Shield } from "lucide-react";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <Image
+            src="/logo.png"
+            className="mx-auto"
+            alt="Logo"
+            width={200}
+            height={200}
+          />
+          <CardTitle className="text-2xl font-bold">WACCBIP</CardTitle>
+          <CardDescription>Sample Management System</CardDescription>
+          <CardTitle className="mt-6 text-2xl font-bold">
+            Welcome Back
+          </CardTitle>
+          <CardDescription>
+            Enter your credentials to access the lab dashboard
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    required
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <Button type="submit" className="w-full ">
+                  Sign In
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <div className="flex gap-4 items-center justify-center">
+        <span className="flex items-center gap-0.5">
+          {" "}
+          <Shield />
+          Secure
+        </span>
+        <span className="flex items-center gap-0.5">
+          {" "}
+          <ChartLine />
+          Real-time
+        </span>
+      </div>
+    </div>
+  );
+}
